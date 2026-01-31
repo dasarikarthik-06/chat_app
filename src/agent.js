48113be6f48a40eb7fc05d7493716a4dbd352e98
Karthik Dasari <karthikdasari@Karthiks-MacBook-Pro.local>
2026-01-31 14:14:25 +0530

    implements basic interaction between two agents

--- src/agent.js
+++ src/agent.js
@@ -1,15 +1,14 @@
-import { encode } from "./helpers.js";
+// import { encode } from "./helpers.js";
 
 const createConnection = async () =>
   await Deno.connect({ port: 8000, transport: "tcp" });
 
 const conversation = async (conn) => {
-  while (true) {
-    await Promise.all([
-      conn.readable.pipeTo(Deno.stdout.writable),
-      Deno.stdin.readable.pipeTo(conn.writable),
-    ]);
-  }
+  console.clear();
+  await Promise.all([
+    conn.readable.pipeTo(Deno.stdout.writable),
+    Deno.stdin.readable.pipeTo(conn.writable),
+  ]);
 };
 
 const main = async () => {
--- src/server.js
+++ src/server.js
@@ -1,22 +1,29 @@
-// import { decode, encode } from "./helpers.js";
+import {  encode } from "./helpers.js";
 
 const createListener = async () =>
   await Deno.listen({ port: 8000, transport: "tcp" });
 
-const handleConversation = async (conn) => {
-  while (true) {
+const handleConversation = async (connections) => {
     await Promise.all([
-      conn.readable.pipeTo(Deno.stdout.writable),
-      Deno.stdin.readable.pipeTo(conn.writable),
+      connections[0].readable.pipeTo(connections[1].writable),
+      connections[1].readable.pipeTo(connections[0].writable),
     ]);
-  }
+
+  connections.forEach(connection => connection.close())
 };
 
 const main = async () => {
+  const connections = [];
   const listener = await createListener();
   for await (const conn of listener) {
-    handleConversation(conn);
+    connections.push(conn);
+    if (connections.length === 1) {
+      conn.write(encode("Waiting for other peer to connect...\n"));
+      continue;
+    }
+    console.clear();
+    connections[0].write(encode("peer joined Session started\n"));
+    handleConversation(connections);
   }
 };
-
 main();
