262c93eabc1a7cbf1f4a234b952aeac8838c027f
Karthik Dasari <karthikdasari@Karthiks-MacBook-Pro.local>
2026-02-01 09:59:16 +0530

    Handles one on one communication for only one pair of clients
    * Formats replies from other client

--- src/agent.js
+++ src/agent.js
@@ -1,10 +1,13 @@
-// import { encode } from "./helpers.js";
+import { encode } from "./helpers.js";
 
 const createConnection = async () =>
   await Deno.connect({ port: 8000, transport: "tcp" });
 
 const conversation = async (conn) => {
   console.clear();
+  const name = prompt("Enter your name: ");
+  const { columns, rows } = Deno.consoleSize();
+  await conn.write(encode(`${name} ${rows} ${columns}`));
   await Promise.all([
     conn.readable.pipeTo(Deno.stdout.writable),
     Deno.stdin.readable.pipeTo(conn.writable),
--- src/server.js
+++ src/server.js
@@ -1,28 +1,56 @@
-import {  encode } from "./helpers.js";
+import { decode, encode } from "./helpers.js";
 
 const createListener = async () =>
   await Deno.listen({ port: 8000, transport: "tcp" });
 
-const handleConversation = async (connections) => {
-    await Promise.all([
-      connections[0].readable.pipeTo(connections[1].writable),
-      connections[1].readable.pipeTo(connections[0].writable),
-    ]);
+const formatMessage = ({ name, cols }) => {
+  console.log(cols);
+  return new TransformStream({
+    transform(chunk, controller) {
+      const msg = decode(chunk);
+      const message = `${name}: ${msg}`;
+      controller.enqueue(encode(message.padStart(cols)));
+    },
+  });
+};
 
-  connections.forEach(connection => connection.close())
+const handleConversation = async (connections) => {
+  await Promise.all([
+    connections[0].conn.readable
+      .pipeThrough(formatMessage(connections[0]))
+      .pipeTo(connections[1].conn.writable),
+
+    connections[1].conn.readable
+      .pipeThrough(formatMessage(connections[1]))
+      .pipeTo(connections[0].conn.writable),
+  ]);
+
+  connections.forEach((connection) => connection.close());
+};
+
+
+
+const read = async (conn) => {
+  const buffer = new Uint8Array(1024);
+  const bytesRead = await conn.read(buffer);
+  const [name, rows, cols] = decode(buffer.slice(0, bytesRead)).split(" ");
+  console.log({ name, rows, cols });
+  return { name, rows, cols };
 };
 
 const main = async () => {
   const connections = [];
   const listener = await createListener();
   for await (const conn of listener) {
-    connections.push(conn);
+    const { name, rows, cols } = await read(conn);
+    connections.push({ name, conn, cols, rows });
     if (connections.length === 1) {
       conn.write(encode("Waiting for other peer to connect...\n"));
       continue;
     }
-    console.clear();
-    connections[0].write(encode("peer joined Session started\n"));
+
+    // console.clear();
+    connections[0].conn.write(encode("peer joined Session started\n"));
     handleConversation(connections);
   }
 };
