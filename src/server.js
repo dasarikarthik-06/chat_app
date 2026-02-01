8adf1b8b07de9bea012dbb0d389105f1d3b2f121
Karthik Dasari <karthikdasari@Karthiks-MacBook-Pro.local>
2026-02-01 12:30:14 +0530

    Implements for handling multi user to chat
    * don't handle ny errors

--- src/server.js
+++ src/server.js
@@ -3,32 +3,10 @@ import { decode, encode } from "./helpers.js";
 const createListener = async () =>
   await Deno.listen({ port: 8000, transport: "tcp" });
 
-const formatMessage = ({ name, cols }) => {
-  console.log(cols);
-  return new TransformStream({
-    transform(chunk, controller) {
-      const msg = decode(chunk);
-      const message = `${name}: ${msg}`;
-      controller.enqueue(encode(message.padStart(cols)));
-    },
-  });
+const formatMessage = (msg, name, cols) => {
+  const message = `${name}: ${msg}`;
+  return message.padStart(cols);
 };
-
-const handleConversation = async (connections) => {
-  await Promise.all([
-    connections[0].conn.readable
-      .pipeThrough(formatMessage(connections[0]))
-      .pipeTo(connections[1].conn.writable),
-
-    connections[1].conn.readable
-      .pipeThrough(formatMessage(connections[1]))
-      .pipeTo(connections[0].conn.writable),
-  ]);
-
-  connections.forEach((connection) => connection.close());
-};
-
-
 
 const read = async (conn) => {
   const buffer = new Uint8Array(1024);
@@ -38,20 +16,32 @@ const read = async (conn) => {
   return { name, rows, cols };
 };
 
+const connections = [];
+
+const handleConversation = async (conn) => {
+  const { name, rows, cols } = await read(conn);
+  connections.push({ name, conn, cols, rows });
+  let personLeft;
+  for await (const chunk of conn.readable) {
+    try {
+      for (const connection of connections) {
+        const message = decode(chunk);
+        if (connection.conn !== conn) {
+          connection.conn.write(encode(formatMessage(message, name, cols)));
+        }
+      }
+    } catch {
+      // personLeft = connection
+      // cons
+    }
+  }
+};
+
 const main = async () => {
-  const connections = [];
   const listener = await createListener();
   for await (const conn of listener) {
-    const { name, rows, cols } = await read(conn);
-    connections.push({ name, conn, cols, rows });
-    if (connections.length === 1) {
-      conn.write(encode("Waiting for other peer to connect...\n"));
-      continue;
-    }
-
     // console.clear();
-    connections[0].conn.write(encode("peer joined Session started\n"));
-    handleConversation(connections);
+    handleConversation(conn);
   }
 };
 main();
